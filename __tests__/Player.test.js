13ee4bc7081ceacbed6a21c08d2357281c6899e6
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-04-05 11:16:47 -0500

    added Player getHealth, isAlive, and reduceHealth methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,9 +4,9 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
 test('creates a player object', () => {
-    const player = new Player('Dave');
+    const player = new Player('Robert');
 
-    expect(player.name).toBe('Dave');
+    expect(player.name).toBe('Robert');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
@@ -14,7 +14,7 @@ test('creates a player object', () => {
 });
 
 test("gets player's stats as an object", () => {
-    const player = new Player('Dave');
+    const player = new Player('Robert');
 
     expect(player.getStats()).toHaveProperty('potions');
     expect(player.getStats()).toHaveProperty('health');
@@ -23,11 +23,40 @@ test("gets player's stats as an object", () => {
 });
 
 test('gets inventory from player or returns false', () => {
-    const player = new Player('Dave');
+    const player = new Player('Robert');
 
     expect(player.getInventory()).toEqual(expect.any(Array));
 
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Robert');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Robert');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtract's from player's health", () => {
+    const player = new Player('Robert');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
 });
